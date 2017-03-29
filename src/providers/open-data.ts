ddb41228de0cde6b3eaa2316f3d1490378bf27a6
cescd6 <cescd6@yahoo.es>
2017-03-29 13:32:44 +0200

    open-data refactoring

--- src/providers/open-data.ts
+++ src/providers/open-data.ts
@@ -13,6 +13,7 @@ export class OpenData {
   public temes : string[] = [];
   private storage: SQLite;
   private isOpen: boolean;
+  private baseUrl: string = 'http://do.diba.cat/api/';
 
 
   constructor(public http: Http, private loadingCtrl: LoadingController) {}
@@ -95,8 +96,8 @@ export class OpenData {
         return this.openBD();
       }).then(() => {
         return this.existsTable(tableName);
-      }).then((existeixTaula) => {
-        if (existeixTaula && columnsNames.length > 0 && values.length > 0){
+      }).then((exists) => {
+        if (exists && columnsNames.length > 0 && values.length > 0){
           let aux: any[] = values;
           for (var i = 0; i < values.length; ++i) {
             for (var j = 0; j < values[i].length; ++j) {
@@ -104,10 +105,10 @@ export class OpenData {
             }
             aux[i] = '(' + values[i].join(',') + ')';
           }
-          let titols = columnsNames.join(',');
-          let valors = aux.join(',');
+          let columns = columnsNames.join(',');
+          let fieldValues = aux.join(',');
           return this.storage.executeSql(
-            'INSERT OR REPLACE INTO ' + tableName + ' (' + titols + ') ' + 'VALUES ' + valors,
+            'INSERT OR REPLACE INTO ' + tableName + ' (' + columns + ') ' + 'VALUES ' + fieldValues,
             []
           )
         }
@@ -119,54 +120,54 @@ export class OpenData {
       return Promise.resolve().then(() => {
         return this.openBD();
       }).then(() => {
-        return this.storage.executeSql('PRAGMA table_info('+tableName+')',[]);
+        return this.storage.executeSql('PRAGMA table_info(' + tableName + ')',[]);
       }).catch((err) => { console.error('ERROR - getColumnsNames: ' + err['message']) });
     }
 
     /*  Retorna el valor del camp <camp> de la taula <nomTaula> amb identificador <id> */
-    private getValueByID(tableName: string, id: string, column: string) {
-      var columnaID;
-      return Promise.resolve().then(() => {
-        return this.openBD();
-      }).then(() => {
-        return this.existsTable(tableName);
-      }).then((existeix) => {
-        if (existeix) return this.getColumnsNames(tableName);
-        else return;
-      }).then((titols : any) => {
-        if (titols.rows.item(0).name) {
-          columnaID = titols.rows.item(0).name;
-          return this.storage.executeSql('SELECT ' + column + ' FROM ' + tableName + ' WHERE ' + columnaID + "='" + id + "'",[]);
-        }
-        else return;
-      }).then((resultat: any) => {
-        if (resultat) return resultat.rows.item(0)[column];
-        else return '';
-      }).catch((err) => { console.error('ERROR - getValueByID: ' + err['message']) });
-    }
+    // private getValueByID(tableName: string, id: string, column: string) {
+    //   var columnaID;
+    //   return Promise.resolve().then(() => {
+    //     return this.openBD();
+    //   }).then(() => {
+    //     return this.existsTable(tableName);
+    //   }).then((existeix) => {
+    //     if (existeix) return this.getColumnsNames(tableName);
+    //     else return;
+    //   }).then((titols : any) => {
+    //     if (titols.rows.item(0).name) {
+    //       columnaID = titols.rows.item(0).name;
+    //       return this.storage.executeSql('SELECT ' + column + ' FROM ' + tableName + ' WHERE ' + columnaID + "='" + id + "'",[]);
+    //     }
+    //     else return;
+    //   }).then((resultat: any) => {
+    //     if (resultat) return resultat.rows.item(0)[column];
+    //     else return '';
+    //   }).catch((err) => { console.error('ERROR - getValueByID: ' + err['message']) });
+    // }
 
     /*  Retorna el nombre d'elements d'un dataset de la BD  */
-    private numElements(tableName: string) {
-      return Promise.resolve().then(() => {
-        return this.openBD();
-      }).then(() => {
-        return this.storage.executeSql('SELECT count(*) FROM ' + tableName, []);
-      }).then((resultat: any) => {
-        if (resultat) return resultat.rows.item(0)['count(*)'];
-        else return '';
-      }).catch((err) => { console.error('ERROR - numElements: ' + err['message']) });
-    }
+    // private numElements(tableName: string) {
+    //   return Promise.resolve().then(() => {
+    //     return this.openBD();
+    //   }).then(() => {
+    //     return this.storage.executeSql('SELECT count(*) FROM ' + tableName, []);
+    //   }).then((resultat: any) => {
+    //     if (resultat) return resultat.rows.item(0)['count(*)'];
+    //     else return '';
+    //   }).catch((err) => { console.error('ERROR - numElements: ' + err['message']) });
+    // }
 
     /*  Esborra la taula <nomTaula> de la BD interna si existeix
         Return d'enllaç: Promise
         Retorn de dades: Promise(Void) */
-    private deleteTable(tableName: string) {
-      return Promise.resolve().then(() => {
-        return this.openBD();
-      }).then(() => {
-        return this.storage.executeSql('DROP TABLE IF EXISTS ' + tableName, [])
-      }).catch((err) => { console.error('ERROR - deleteTable: ' + err['message']) });
-    }
+    // private deleteTable(tableName: string) {
+    //   return Promise.resolve().then(() => {
+    //     return this.openBD();
+    //   }).then(() => {
+    //     return this.storage.executeSql('DROP TABLE IF EXISTS ' + tableName, [])
+    //   }).catch((err) => { console.error('ERROR - deleteTable: ' + err['message']) });
+    // }
 
     /*  tslint:enable */
   /************************************* BD MANAGEMENT END **********************************************/
@@ -186,32 +187,32 @@ export class OpenData {
     /*  Retorna la última data de modificació d'un dataset a la API
         Return d'enllaç: Promise
         Retorn de dades: Promise(resultat -> String) / resultat pot ser: { <data> | undefined } */
-    private getUpdateDateAPI(datasetName: string, token: string) {
-      return new Promise((resolve) => {
-        this.http.get('http://do.diba.cat/api/info/datasets/dataset/' + datasetName + '/token/' + token)
-        .timeout(5000)
-        .map(res => res.json()['modificacio'])
-        .subscribe(data => { resolve(data) }, err => { resolve(undefined) });
-      }).catch((err) => { console.log('getUpdateDateAPI - Error: ' + err); return undefined; });
-    }
+    // private getUpdateDateAPI(datasetName: string, token: string) {
+    //   return new Promise((resolve) => {
+    //     this.http.get(this.baseUrl + 'info/datasets/dataset/' + datasetName + '/token/' + token)
+    //     .timeout(5000)
+    //     .map(res => res.json()['modificacio'])
+    //     .subscribe(data => { resolve(data) }, err => { resolve(undefined) });
+    //   }).catch((err) => { console.log('getUpdateDateAPI - Error: ' + err); return undefined; });
+    // }
 
     /*  Retorna la informació d'un dataset <nomDataset> de la API
         Return d'enllaç: Promise
         Return de dades: Promise(resultat -> array) */
-    private getDataSetAPIContent(datasetName: string, token: string, camp_ord: string) {
+    private getDatasetAPIContent(datasetName: string, token: string, camp_ord: string) {
       return new Promise((resolve) => {
-        this.http.get('http://do.diba.cat/api/dataset/' + datasetName + '/format/JSON/ord-' + camp_ord + '/token/' + token)
+        this.http.get(this.baseUrl + 'dataset/' + datasetName + '/format/JSON/ord-' + camp_ord + '/token/' + token)
         .timeout(5000)
         .map(res => res.json()['elements'])
         .subscribe(data => { resolve(data) });
       })
-      .catch((err) => { console.error('getDataSetAPIContent - Error: ' + err); });
+      .catch((err) => { console.error('getDatasetAPIContent - Error: ' + err); });
     }
 
     /*  Retorna l'informació d'un contentType <contentType>, pel municipi <ine> especificat */
     private getContentTypeContent(contentTypeName: string, ine: string) {
       return new Promise((resolve) => {
-        this.http.get('http://do.diba.cat/api/tipus/' + contentTypeName + '/camp-rel_municipis/' + ine)
+        this.http.get(this.baseUrl + 'tipus/' + contentTypeName + '/camp-rel_municipis/' + ine)
         .timeout(5000)
         .map((data) => data.json().datasets)
         .subscribe((data) => { resolve(data) });
@@ -252,13 +253,13 @@ export class OpenData {
             else subIndex.push('empty');
 
             for (var z = 0; z < subIndex.length; ++z) {
-              let contingutCamp;
-              if (subIndex[0] == 'empty') contingutCamp = APIData[i][index];
-              else contingutCamp = APIData[i][index][subIndex[z]];
+              let fieldContent;
+              if (subIndex[0] == 'empty') fieldContent = APIData[i][index];
+              else fieldContent = APIData[i][index][subIndex[z]];
               // Per no generar problemes en l'inserció a la BD
-              contingutCamp = contingutCamp.replace(/,/g,';');
-              contingutCamp = contingutCamp.replace(/'/g,'¿');
-              camp.push("'" + contingutCamp + "'");
+              fieldContent = fieldContent.replace(/,/g,';');
+              fieldContent = fieldContent.replace(/'/g,'¿');
+              camp.push("'" + fieldContent + "'");
             }
           }
           content.push(camp);
@@ -320,7 +321,7 @@ export class OpenData {
     /*  Carrega desde la API de dades Obertes el dataset Municipis amb els elements ordenats per municipi transliterat */
     public loadMunicipis(loading: any) {
       return Promise.resolve().then(() => {
-        return this.getDataSetAPIContent('municipis','','municipi_transliterat');
+        return this.getDatasetAPIContent('municipis','','municipi_transliterat');
       }).then((contingutAPI) => {
         return this.contentFormat(contingutAPI);
       }).then((contingutFormatejat: any) => {
@@ -353,7 +354,7 @@ export class OpenData {
       let munFil : any;
       let comFil : any;
       return new Promise(resolve => {
-        this.http.get('http://do.diba.cat/api/dataset/municipis/format/JSON/ord-municipi_transliterat/camp-municipi_nom-like/'+searchValue)
+        this.http.get(this.baseUrl + 'dataset/municipis/format/JSON/ord-municipi_transliterat/camp-municipi_nom-like/'+searchValue)
         .map(res => res.json())
         .subscribe(data => {
           resolve(data);
@@ -362,7 +363,7 @@ export class OpenData {
       }).then((municipis) => {
         munFil = municipis;
         return new Promise(resolve => {
-          this.http.get('http://do.diba.cat/api/dataset/municipis/format/JSON/ord-municipi_transliterat/camp-comarca_nom-like/'+searchValue)
+          this.http.get(this.baseUrl + 'dataset/municipis/format/JSON/ord-municipi_transliterat/camp-comarca_nom-like/'+searchValue)
           .map(res => res.json())
           .subscribe(data => {
             resolve(data);
