98f713b401366400fa000a4e441b343b0b1da97b
Francesc Giralt <giraltqf@diba.cat>
2017-05-26 14:03:22 +0200

    poi-detail OK

--- src/pages/pois-detail/pois-detail.ts
+++ src/pages/pois-detail/pois-detail.ts
@@ -1,22 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, FabContainer } from 'ionic-angular';
 
-/*
-  Generated class for the PoisDetail page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-pois-detail',
   templateUrl: 'pois-detail.html'
 })
 export class PoisDetailPage {
+  private poi: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams
+  ) {
+      this.poi = this.navParams.data.poi;
+  }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PoisDetailPage');
+  share(socialNet: string, fab: FabContainer) {
+    console.log("Sharing in" + socialNet);
+    fab.close();
+  }
+
+  openMap() {
+    console.log("Open map");
+  }
+
+  arrayToString(text: any){
+      return text.join(", ");
+  };
+
+  openMail(email) {
+    window.open(`mailto:${email}`, '_system', 'location=yes');
+  }
+
+  openTel(telf) {
+    window.open('tel:'+telf);
+  }
+
+  openLink(link) {
+    console.log('openLink: ' + link);
+    console.log(link);
+    window.open(`${link}`, '_system', 'location=yes');
   }
 
 }
