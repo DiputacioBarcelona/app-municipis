88d573e69da37670c7e69244221b99c9ce9d6042
Francesc Giralt <giraltqf@diba.cat>
2017-06-08 16:20:34 +0200

    first version dateFormat

--- src/pipes/date-format.ts
+++ src/pipes/date-format.ts
@@ -8,10 +8,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateFormat implements PipeTransform {
 
     transform(value:any, arg1:any):any {
-        console.log('Data ini: '+ value);
-        console.log('Data fi: '+ arg1);
+        let retValue = '';
+
+        let date_ini: Date = new Date(value);
+        let date_fi: Date = new Date(arg1);
+        /*console.log('Data ini: '+ date_ini.toLocaleString());
+        console.log('Data fi: '+ date_fi.toLocaleString());
+        console.log('getDay: '+ date_ini.getDay);
+        console.log('getHours: '+ date_ini.getHours);
+        console.log('getSecords: '+ date_ini.getSeconds);*/
         
-        return value.substring(0,10);
+        retValue = date_ini.toLocaleString() + ' - ' + date_fi.toLocaleString();
+
+        return retValue;
         
     }
 }
